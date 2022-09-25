cfdd82fae6af59601b9bf36cb7b30d696e02676b
Ajay <ap221882@gmail.com>
2022-09-25 14:10:03 +0530

    feat: designed homepage and added globalstyles

--- ajay-portfolio/src/App.js
+++ ajay-portfolio/src/App.js
@@ -1,9 +1,11 @@
+import GlobalStyle from "./globalStyles";
 import Navbar from "./components/Navbar/Navbar";
 import Homepage from "./pages/Homepage/Homepage";
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Navbar />
       <Homepage />
     </>
--- ajay-portfolio/src/pages/Homepage/Homepage.jsx
+++ ajay-portfolio/src/pages/Homepage/Homepage.jsx
@@ -3,10 +3,37 @@ import style from "styled-components";
 
 const StyledHomepage = style.div`
 border:2px solid red;
-height:100vh`;
+height:100vh;
+// display: grid;
+// grid-template-columns:1fr 1fr;
+position:relative;
+.dark-side{
+  height:100vh;
+  float:left;
+  width:65%;
+  position:absolute;
+  background:black;
+  z-index:2;
+  clip-path:polygon(0% 0%, 100% 0%, 80% 100%, 0% 100%);
+  clip-path:polygon(0% 0%, 80% 0%, 100% 100%, 0% 100%);
+}
+.light-side{
+  height:100vh;
+  width:50%;
+  left:50%;
+  float:right;
+  position:absolute;
+  background:white;
+}
+`;
 
 const Homepage = () => {
-  return <StyledHomepage>Homepage</StyledHomepage>;
+  return (
+    <StyledHomepage>
+      <div className="dark-side"></div>
+      <div className="light-side"></div>
+    </StyledHomepage>
+  );
 };
 
 export default Homepage;
