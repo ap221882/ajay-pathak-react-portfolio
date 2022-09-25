a2292901586197d42fbbdc06b12ea9436ff64b92
Ajay <ap221882@gmail.com>
2022-09-25 16:30:51 +0530

    feat: added logo and made navbar sticky

--- ajay-portfolio/src/components/Navbar/Navbar.jsx
+++ ajay-portfolio/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,42 @@
 import React from "react";
+import styled from "styled-components";
+import logo from "../../assets/logo.png";
+
+const StyledNavbar = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+  justify-items: start;
+  position: sticky;
+  top: 0;
+  z-index: 3;
+  .logo-container {
+    height: 40px;
+    width: 66px;
+  }
+  .buttons-container {
+    ul {
+      display: grid;
+      grid-template-columns: repeat(5, auto);
+    }
+  }
+`;
 
 const Navbar = () => {
   return (
-    <>
-      <ul>
-        <li>Home</li>
-        <li>About us</li>
-        <li>Service</li>
-        <li>Portfolio</li>
-        <li>Contact Us</li>
-      </ul>
-    </>
+    <StyledNavbar>
+      <div className="logo-container">
+        <img src={logo} alt="" />
+      </div>
+      <div className="buttons-container">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Service</li>
+          <li>Portfolio</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+    </StyledNavbar>
   );
 };
 
