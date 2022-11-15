a57adc002e5a6a70fb895d31d45d639ea6fbd14b
ajay <ap221882@gmail.com>
2022-11-15 09:08:33 +0530

    removed extra css

--- ajay-portfolio/src/pages/AboutPage/AboutPage.jsx
+++ ajay-portfolio/src/pages/AboutPage/AboutPage.jsx
@@ -4,10 +4,31 @@ import styled from "styled-components";
 const StyledAbout = styled.div`
   height: 100vh;
   background-color: gray;
+  .dark-side {
+    height: 100vh;
+    float: left;
+    width: 55%;
+    position: absolute;
+    background: black;
+    z-index: 1;
+    clip-path: polygon(0% 0%, 80% 0%, 100% 100%, 0% 100%);
+  }
+  .light-side {
+    width: 50%;
+    left: 50%;
+    float: right;
+    position: absolute;
+    background: white;
+  }
 `;
 
 const AboutPage = () => {
-  return <StyledAbout>AboutPage</StyledAbout>;
+  return (
+    <StyledAbout>
+      <div className='dark-side'></div>
+      <div className='light-side'></div>
+    </StyledAbout>
+  );
 };
 
 export default AboutPage;
