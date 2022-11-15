1e14031943634ea1b2ccc980666ecd48fcfb728d
ajay <ap221882@gmail.com>
2022-11-15 20:12:06 +0530

    added dummy data to cards

---
+++ ajay-portfolio/src/containers/ServicesContainer/Cards.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { FaBeer } from "react-icons/fa";
+
+const CardData = [
+  {
+    id: 1,
+    icon: "FaBeer",
+    title: "Ux designer",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore cumque blanditiis porro aspernatur atque, inventore qui? Fugiat illum assumenda voluptatem.",
+  },
+  {
+    id: 2,
+    icon: "FaBeer",
+    title: "Ux designer",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore cumque blanditiis porro aspernatur atque, inventore qui? Fugiat illum assumenda voluptatem.",
+  },
+  {
+    id: 3,
+    icon: "FaBeer",
+    title: "Ux designer",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore cumque blanditiis porro aspernatur atque, inventore qui? Fugiat illum assumenda voluptatem.",
+  },
+  {
+    id: 4,
+    icon: "FaBeer",
+    title: "Ux designer",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore cumque blanditiis porro aspernatur atque, inventore qui? Fugiat illum assumenda voluptatem.",
+  },
+  {
+    id: 5,
+    icon: "FaBeer",
+    title: "Ux designer",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore cumque blanditiis porro aspernatur atque, inventore qui? Fugiat illum assumenda voluptatem.",
+  },
+  {
+    id: 6,
+    icon: "FaBeer",
+    title: "Ux designer",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore cumque blanditiis porro aspernatur atque, inventore qui? Fugiat illum assumenda voluptatem.",
+  },
+];
+
+const Cards = () => {
+  return <div>Cards</div>;
+};
+
+export default Cards;
--- ajay-portfolio/src/containers/ServicesContainer/ServicesContainer.jsx
+++ ajay-portfolio/src/containers/ServicesContainer/ServicesContainer.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import styled from "styled-components";
+import Cards from "./Cards";
 
 const StyledServicesContainer = styled.div`
-  padding-top: 44px;
+  padding-top: 8rem;
+  display: flex;
+  flex-direction: column;
+  .service-heading {
+    text-align: center;
+    font-size: 30px;
+    text-transform: capitalize;
+  }
 `;
 
 const ServicesContainer = () => {
-  return <StyledServicesContainer>ServicesContainer</StyledServicesContainer>;
+  return (
+    <StyledServicesContainer>
+      <h2 className='service-heading'>Services</h2>
+      <Cards />
+    </StyledServicesContainer>
+  );
 };
 
 export default ServicesContainer;
