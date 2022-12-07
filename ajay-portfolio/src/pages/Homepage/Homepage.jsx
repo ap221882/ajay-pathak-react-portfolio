9e655757a5124a020f4d81ed1a1334f454b85101
ajay <ap221882@gmail.com>
2022-12-07 13:04:34 +0530

    feat: restructured

--- ajay-portfolio/src/pages/Homepage/Homepage.jsx
+++ ajay-portfolio/src/pages/Homepage/Homepage.jsx
@@ -1,37 +1,6 @@
 import React from "react";
-import styled from "styled-components";
 import IntroContainer from "../../containers/IntroContainer/IntroContainer";
-
-const StyledHomepage = styled.div`
-  height: calc(100vh - 60px);
-  position: relative;
-  .dark-side {
-    height: calc(100vh - 60px);
-    float: left;
-    width: 55%;
-    position: absolute;
-    background: black;
-    z-index: 1;
-    clip-path: polygon(0% 0%, 100% 0%, 80% 100%, 0% 100%);
-    clip-path: polygon(0% 0%, 80% 0%, 100% 100%, 0% 100%);
-  }
-  .light-side {
-    width: 50%;
-    left: 50%;
-    float: right;
-    position: absolute;
-    background: white;
-  }
-`;
-
-const StyledIntroContainer = styled.div`
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  z-index: 2;
-  color: white;
-  width: 100vw;
-`;
+import { StyledHomepage, StyledIntroContainer } from "./Homepage.styles";
 
 const Homepage = () => {
   return (
